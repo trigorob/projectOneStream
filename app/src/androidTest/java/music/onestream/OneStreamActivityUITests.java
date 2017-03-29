f6a521112d5e96fbefd0349c6eeb61984103fcce
Preston Chang <preston@eco-service.ca>
2017-03-29 12:55:02 -0700

    Created tests for switching tabs on onestream activity

--- app/src/androidTest/java/music/onestream/OneStreamActivityUITests.java
+++ app/src/androidTest/java/music/onestream/OneStreamActivityUITests.java
@@ -10,11 +10,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import music.onestream.activity.OneStreamActivity;
+import music.onestream.util.Constants;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.StringEndsWith.endsWith;
 //import android.support.test.espresso.Espresso;
 
 
@@ -27,7 +36,7 @@ public class OneStreamActivityUITests {
             OneStreamActivity.class);
 
     @Test
-    public void checkUI() {
+    public void verifyPlaybackControls() {
 
         onView(withText("OneStream")).check(ViewAssertions.matches(isDisplayed()));
 
@@ -38,6 +47,111 @@ public class OneStreamActivityUITests {
         onView(withId(R.id.Prev)).check(ViewAssertions.matches(isDisplayed()));
         onView(withId(R.id.Next)).check(ViewAssertions.matches(isDisplayed()));
         onView(withId(R.id.seekBar)).check(ViewAssertions.matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void navigateToLocalTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_Local)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_Local)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_Local)).check(ViewAssertions.matches(isSelected()));
+
+    }
+
+    @Test
+    public void navigateToSpotifyTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_Spotify)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_Spotify)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_Spotify)).check(ViewAssertions.matches(isSelected()));
+
+    }
+
+    @Test
+    public void navigateToSoundCloudTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_SoundCloud)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_SoundCloud)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_SoundCloud)).check(ViewAssertions.matches(isSelected()));
+
+    }
+
+    @Test
+    public void navigateToPlaylistsTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_Playlists)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_Playlists)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_Playlists)).check(ViewAssertions.matches(isSelected()));
+
+    }
+
+    @Test
+    public void navigateToArtistsTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_Artists)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_Artists)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_Artists)).check(ViewAssertions.matches(isSelected()));
+
+    }
+
+    @Test
+    public void navigateToAlbumsTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_Albums)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_Albums)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_Albums)).check(ViewAssertions.matches(isSelected()));
+
+    }
+
+    @Test
+    public void navigateToGenresTabTest() {
+
+        //Click Local tab
+        onView(allOf(withClassName(endsWith("TabView")),
+                withChild(withText(Constants.OneStream_Genre)),
+                withParent(withParent(withId(R.id.tabs)))
+        )).perform(scrollTo()).perform(click());
+
+        //Check if Local tab is selected
+        onView(withText(Constants.OneStream_Genre)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText(Constants.OneStream_Genre)).check(ViewAssertions.matches(isSelected()));
 
     }
 }
