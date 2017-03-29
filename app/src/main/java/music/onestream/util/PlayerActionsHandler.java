348121d7d7b8cf4d8dfef522d2b8b475bc68b8cd
Russ <ruspearce@gmail.com>
2017-03-29 14:36:27 -0700

    minor tweaks to adapter scrolling

--- app/src/main/java/music/onestream/util/PlayerActionsHandler.java
+++ app/src/main/java/music/onestream/util/PlayerActionsHandler.java
@@ -510,8 +510,8 @@ public class PlayerActionsHandler implements SeekBar.OnSeekBarChangeListener,
 
         if (viewingCurrentList())
         {
-            ((SongAdapter) mainList.getAdapter()).setSelected(songIndex);
-            mainList.smoothScrollToPosition(songIndex);
+            ((SongAdapter) mainList.getAdapter()).setSelected(currentSong);
+            mainList.smoothScrollToPositionFromTop(songIndex, 0, 0);
             mainList.setItemChecked(songIndex, true);
         }
         fabIO.setImageResource(R.drawable.pause);
@@ -649,8 +649,8 @@ public class PlayerActionsHandler implements SeekBar.OnSeekBarChangeListener,
             serviceIconPausePlay(true);
             if (viewingCurrentList())
             {
-                ((SongAdapter) mainList.getAdapter()).setSelected(songIndex);
-                mainList.smoothScrollToPosition(songIndex);
+                ((SongAdapter) mainList.getAdapter()).setSelected(currentSong);
+                mainList.smoothScrollToPositionFromTop(songIndex, 0, 0);
                 mainList.setItemChecked(songIndex, true);
             }
         }
